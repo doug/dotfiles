648ecb96c4d74f093077288f4368d0746797289a
Doug Fritz <dougfritz@gmail.com>
2012-08-29 13:36:25 -0700

    setup.sh updated installers

--- setup.sh
+++ setup.sh
@@ -5,15 +5,18 @@ if [[ "$platform" == "Linux" || "$platform" == "Darwin" ]]; then
 chsh -s `which zsh`
 
 if [[ "$platform" == "Linux" ]]; then
-
-echo "Setting up Linux"
-
+  echo "Setting up Linux"
+  sudo apt-get install gfortran
 elif [[ "$platform" == "Darwin" ]]; then
-
-echo "Setting up Mac"
-curl -fsS https://raw.github.com/mxcl/homebrew/go | bash --ruby
-
+  echo "Setting up Mac"
+  curl -fsS https://raw.github.com/mxcl/homebrew/go | bash --ruby
+  brew install wget node go ffmpeg gfortran macvim tmux
 fi
+
+# Download fonts I like
+mkdir -p fonts
+./gfont.py "http://googlefontdirectory.googlecode.com/hg/ofl/dosis/METADATA.json"
+./gfont.py "http://googlefontdirectory.googlecode.com/hg/ofl/anonymouspro/METADATA.json"
 
 # Install rvm
 curl -L https://get.rvm.io | bash -s stable --ruby
@@ -23,18 +26,23 @@ curl -kL http://xrl.us/pythonbrewinstall | bash
 
 source $HOME/.rvm/scripts/rvm
 source $HOME/.pythonbrew/etc/bashrc
+# copy and install these dotfiles
 rake install
 
-pythonbrew install 2.7.3
+if [[ "$platform" == "Linux" ]]; then
+  pythonbrew install 2.7.3
+elif [[ "$platform" == "Darwin" ]]; then
+  sudo pythonbrew install --framework --force --configure="--with-universal-archs=intel --enable-universalsdk" 2.7.3
+  sudo chown -R dougfritz $HOME/.pythonbrew
+fi
 pythonbrew switch 2.7.3
-pip install readline
+easy_install readline
 pip install ipython
+pip install pil
 pip install requests
+pip install numpy
+pip install scipy
 
-# Download fonts I like
-mkdir -p fonts
-./gfont.py "http://googlefontdirectory.googlecode.com/hg/ofl/dosis/METADATA.json"
-./gfont.py "http://googlefontdirectory.googlecode.com/hg/ofl/anonymouspro/METADATA.json"
 
 
 
