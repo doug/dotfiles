399d3f45e55981467a7ef4387152fe183e9ab9ec
Aki Rodic <aleksandar.xyz@gmail.com>
2014-08-28 11:08:05 -0700

    updated fonts for osx

--- download-fonts.zsh
+++ download-fonts.zsh
@@ -1,7 +1,15 @@
-mkdir -p $HOME/.fonts
-for x in Roboto Anonymous-Pro source-sans-pro open-sans Raleway quicksand dosis source-code-pro code Museo League-Gothic; do 
+
+if [[ "$OSTYPE" == linux* ]]; then
+  FONTDIR=$HOME/.fonts
+elif
+  [[ "$OSTYPE" == darwin* ]]; then
+  FONTDIR=$HOME/Library/Fonts
+fi
+
+mkdir -p $FONTDIR
+for x in Roboto Anonymous-Pro source-sans-pro open-sans Raleway quicksand dosis source-code-pro code Museo League-Gothic; do
   curl -O http://www.fontsquirrel.com/fonts/download/$x
-  unzip -o $x -d $HOME/.fonts
+  unzip -o $x -d $FONTDIR
   rm $x
 done
 
